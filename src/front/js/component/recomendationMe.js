4b7318747b333fec20e709980a37da538cf6c508
Jose Geek <josemariarodriguez78@gmail.com>
2024-01-16 14:58:08 +0100

    Mejorando imagenes y color de fondo

--- src/front/js/component/recomendationMe.js
+++ src/front/js/component/recomendationMe.js
@@ -8,7 +8,7 @@ import card_3 from "../../../../public/img/card_3.png";
 const RecomendationMe = () => {
   return (
     <section className="recomendationMe">
-      <div class="curved-div upper">
+      <div className="curved-div upper bgSvgDiv">
         <svg viewBox="0 0 1440 319">
           <path
             fill="#005F71"
@@ -67,7 +67,7 @@ const RecomendationMe = () => {
         </h5>
         <svg viewBox="0 0 1440 319">
           <path
-            fill="#fff"
+            fill="rgb(245, 245, 230)"
             fill-opacity="1"
             d="M0,32L48,80C96,128,192,224,288,224C384,224,480,128,576,90.7C672,53,768,75,864,96C960,117,1056,139,1152,149.3C1248,160,1344,160,1392,160L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
           ></path>
